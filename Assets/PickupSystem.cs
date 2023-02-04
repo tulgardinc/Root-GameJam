1351516fc93cce52b0ae020dd6a5f234fe0e1c96
Kaolver <77968457+Kaolver@users.noreply.github.com>
2023-02-04 22:14:07 +0100

    Button Door Pressure Plate and Elevator done

--- Assets/PickupSystem.cs
+++ Assets/PickupSystem.cs
@@ -7,6 +7,12 @@ public class PickupSystem : MonoBehaviour
     bool isItemInRange;
     [SerializeField] float canGetRadius;
     [SerializeField] LayerMask itemLayer;
+    [SerializeField] Transform itemPos;
+    bool itemPicked = false;
+    bool isClicked = false;
+    
+
+    GameObject temp;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +28,44 @@ public class PickupSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(isItemInRange);
         if (isItemInRange)
         {
-            Debug.Log("Touching");
+            Debug.Log("inRange");
+            if (Input.GetKeyDown(KeyCode.R) && !isClicked)
+            {
+                isClicked= true;
+                Debug.Log("picked");
+                itemPicked = true;
+                temp.GetComponent<Rigidbody2D>().gravityScale = 0;
+               
+               
+            } else if (Input.GetKeyDown(KeyCode.R) && isClicked)
+            {
+                isClicked = false;
+                itemPicked = false;
+                temp.GetComponent<Rigidbody2D>().gravityScale = 1;
+            }
 
+            if (itemPicked)
+            {
+                temp.transform.position = itemPos.transform.position;
+            }
+
+        }
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isItemInRange)
+        {
+            int layer = collision.gameObject.layer;
+
+            if (layer == LayerMask.NameToLayer("Tree"))
+            {
+                temp = collision.gameObject;
+            }
+            
         }
     }
 }
